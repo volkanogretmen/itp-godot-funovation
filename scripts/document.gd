b2e84ba19864cb0804778db9a288a75058b8cfcb
Volkan <volkanogretmen@gmail.com>
2024-06-13 15:13:54 +0200

    HIJ DOET HET

--- scripts/document.gd
+++ scripts/document.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 # Velocity of the Area2D
 var velocity = Vector2()
+var sound = preload("res://audio/document_score.mp3")
 
 func _ready():
 	# Initialization code here, if needed
@@ -16,5 +17,9 @@ func _process(delta):
 
 
 func _on_body_entered(body):
-	print("+1 document!")
+	print('1')
+	var audio = AudioStreamPlayer.new()
+	audio.stream = sound
+	add_sibling(audio)
+	audio.play()
 	queue_free()
