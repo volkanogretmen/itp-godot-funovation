660b296a9482ec933a1bd95fd9716459f8fd4fda
Volkan <volkanogretmen@gmail.com>
2024-06-13 15:51:42 +0200

    pushed

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,7 +1,19 @@
 extends Node
 
 var score = 0
+@export var score_label_path: NodePath  # Exported NodePath for ScoreLabel
+@onready var score_label: Label = null
+
+func _ready():
+	score_label = get_node(score_label_path) as Label
+	if score_label == null:
+		print("Error: score_label is null")
+	else:
+		print("score_label found:", score_label)
 
 func add_point():
 	score += 1
-	print(score)
+	if score_label:
+		score_label.text = "Score: " + str(score) + " "
+	else:
+		print("Error: score_label is null when adding point")
